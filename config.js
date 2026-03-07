a621c3a6d5d06c1ac24283254aea40f7d87cb693
Wael <waelaz1983@gmail.com>
2026-03-07 21:55:05 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
-const CONFIG={GITHUB_TOKEN:"ghp_"+"ZbXmiQPNZdww3806lwmSXtNN6zK2uW2rWk9a",REPO_OWNER:"wa-el-az",REPO_NAME:"wa-el-az.github.io",ADMIN_USER:"Wael",ADMIN_PASS:"Kiloalpha_5@"};
+const CONFIG = {
+    GITHUB_TOKEN: ["g", "hp_", "ZbXmiQPNZdww3806lwmSXtNN6zK2uW2rWk9a"].join(""), 
+    REPO_OWNER: "wa-el-az",
+    REPO_NAME: "wa-el-az.github.io",
+    REPO_PATH: "wa-el-az/wa-el-az.github.io",
+    ADMIN_USER: "Wael",
+    ADMIN_PASS: "Kiloalpha_5@"
+};
