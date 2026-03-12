c69d7d45f17537724e057705de17372c24f7af1d
Wael Amrani Zerrifi <waelaz1983@gmail.com>
2026-03-12 21:32:41 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     GITHUB_TOKEN: ["g", "hp_", "ZbXmiQPNZdww3806lwmSXtNN6zK2uW2rWk9a"].join(""), 
     REPO_OWNER: "wa-el-az",
-    REPO_NAME: "wa-el-az.github.io",
-    REPO_PATH: "wa-el-az/wa-el-az.github.io",
+    REPO_NAME: "WaelOS",
+    REPO_PATH: "wa-el-az/WaelOS",
     ADMIN_USER: "Wael",
     ADMIN_PASS: "Kiloalpha_5@"
 };
